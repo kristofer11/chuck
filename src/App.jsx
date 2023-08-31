967199deb44df53b940e688171bcb3196125ffaa
Kris Hvattum <klhvattum@live.com>
2023-08-31 13:57:01 -0700

    add favicon

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import HomeImages from './components/HomeImages';
 import Footer from './components/Footer';
 import HomePriorities from './components/HomePriorities';
 import AboutChuck from './components/AboutChuck';
+import Delete from './components/Delete'
 
 function App() {
 
