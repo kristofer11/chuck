e9163d076651ccd30468007b9240962561f7a709
Kris Hvattum <klhvattum@live.com>
2023-08-30 22:39:19 -0700

    style hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,19 +1,23 @@
 // import HeroImg from '../assets/IMG_2532.png'
-import HeroImg from '../assets/mainPortrait.jpg'
+import HeroImg from '../assets/Untitled design (1).png'
 
 const Hero = () => {
     return (
         <section className="hero-section">
             <div className="hero-container">
-                <img className='hero-img' src={HeroImg} alt="Portrait of Chuck Whitfield, candidate for Monroe School Board Director, district 2" />
                 <div className='hero-text'>
-                    <h1>Experience, Commitment, Community: Chuck Whitfield for Monroe Schools</h1>
-                    <p>Over the last four years it has
+                        <h1>Experience,</h1> 
+                        <h1>Commitment, </h1>
+                        <h1>Community: </h1>
+                        <h2>Chuck Whitfield for Monroe Schools</h2>
+                    {/* <p>Over the last four years it has
                         been my pleasure to serve as board director during some difficult times.
                         During these hard times we worked together overcoming hardships and finding
-                        solutions to move forward. I've served on curriculum committee, encouraging community input and choosing robust curriculum, and as the legislative representative, building relationships with lawmakers and advocating for our students.</p>
+                        solutions to move forward. I've served on curriculum committee, encouraging community input and choosing robust curriculum, and as the legislative representative, building relationships with lawmakers and advocating for our students.</p> */}
                 </div>
+                <img className='hero-img' src={HeroImg} alt="Portrait of Chuck Whitfield, candidate for Monroe School Board Director, district 2" />
             </div>
+
 
         </section>
     )
