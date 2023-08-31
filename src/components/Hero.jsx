f4547d34943995d3a1b1bb46191a006a2f4ae55c
Kris Hvattum <klhvattum@live.com>
2023-08-31 12:07:37 -0700

    change to new images for hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,15 @@
 // import HeroImg from '../assets/IMG_2532.png'
-import HeroImg from '../assets/Untitled design (1).png'
+import HeroImg from '../assets/Headshot.png'
 
 const Hero = () => {
     return (
         <section className="hero-section">
             <div className="hero-container">
                 <div className='hero-text'>
-                        <h1>Experience,</h1> 
-                        <h1>Commitment, </h1>
-                        <h1>Community: </h1>
-                        <h2>Chuck Whitfield for Monroe School Board | District 2</h2>
+                        <h1>Experience</h1> 
+                        <h1 className='commitment'>Commitment</h1>
+                        <h1>Community</h1>
+                        {/* <h2>Chuck Whitfield for Monroe School Board | District 2</h2> */}
                     {/* <p>Over the last four years it has
                         been my pleasure to serve as board director during some difficult times.
                         During these hard times we worked together overcoming hardships and finding
