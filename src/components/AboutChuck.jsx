9b26aa26f98a55a7c739eb1aee1959610827f290
Kris Hvattum <klhvattum@live.com>
2023-08-31 13:43:31 -0700

    style about section

--- src/components/AboutChuck.jsx
+++ src/components/AboutChuck.jsx
@@ -5,9 +5,16 @@ const AboutChuck = () => {
             <div className="about-chuck-text">
 
                 <h2>About Chuck</h2>
-                <p>I've lived in Monroe my entire life. Igraduated from Monroe High School, enlisted into the Army, graduated from college with a business degree and currently operate a vehicle-licensing agency in Monroe. Through these stages of life I have learned how to be a leader who leads by example.</p>
-                <p>Over the last four years it has been my pleasure to serve as board director during some difficult times. During these hard times we worked together overcoming hardships and finding solutions to move forward. I've served on curriculum committee, encouraging community input and choosing robust curriculum, and as the legislative representative, building relationships with lawmakers and advocating for our students.</p>
-                <p>I remain committed to prioritizing the education of our students, collaborating with thecommunity building partnerships and share ideas, ensuring teachers/staff are supported and equipped with tools they need to provide a quality education for every student, partnering with parents/guardians in the education of their children and continuing to be fiscally responsible with taxpayerdollars.</p>
+                <p>I've lived in Monroe my entire life. I graduated from Monroe High School, enlisted into the Army, graduated from college with a business degree and currently operate a vehicle-licensing agency in Monroe. Through these stages of life I have learned how to be a leader who leads by example.</p>
+                <p>Over the last four years it has been my pleasure to serve as board director during some difficult times. During these hard times we worked together overcoming hardships and finding solutions to move forward. I've served on the curriculum committee, encouraging community input and choosing robust curriculum, and as the legislative representative, building relationships with lawmakers and advocating for our students.</p>
+                <p><strong>I remain committed to the following:</strong>
+                    <ul>
+                        <li>Prioritizing the education of our students, collaborating with the community</li>
+                        <li>Building partnerships and sharing ideas - ensuring teachers and staff are supported and equipped with tools they need to provide a quality education for every student</li>
+                        <li>Partnering with parents/guardians in the education of their children</li>
+                        <li>Continuing to be fiscally responsible with taxpayer dollars.</li>
+                    </ul>
+                    </p>
                 <p>I will continue to serve our students, staff and community fairly and equitably, ensuring everyone has a seat at the table, while keeping student wellness and educational success at the forefront of all my decisions.</p>
             </div>
 
