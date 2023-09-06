9b2beaeac1a5eec21ca929c2b2501bcac4c575f4
Kris Hvattum <klhvattum@live.com>
2023-09-06 14:31:55 -0700

    style lists

--- src/components/AboutChuck.jsx
+++ src/components/AboutChuck.jsx
@@ -7,7 +7,7 @@ const AboutChuck = () => {
                 <h2>About Chuck</h2>
                 <p>I've lived in Monroe my entire life. I graduated from Monroe High School, enlisted into the Army, graduated from college with a business degree and currently operate a vehicle-licensing agency in Monroe. Through these stages of life I have learned how to be a leader who leads by example.</p>
                 <p>Over the last four years it has been my pleasure to serve as board director during some difficult times. During these hard times we worked together overcoming hardships and finding solutions to move forward. I've served on the curriculum committee, encouraging community input and choosing robust curriculum, and as the legislative representative, building relationships with lawmakers and advocating for our students.</p>
-                <p><strong>I remain committed to the following:</strong>
+                <p style={{textAlign: 'center'}}><strong>I remain committed to the following:</strong>
                     <ul>
                         <li>Prioritizing the education of our students, collaborating with the community</li>
                         <li>Building partnerships and sharing ideas - ensuring teachers and staff are supported and equipped with tools they need to provide a quality education for every student</li>
@@ -15,7 +15,7 @@ const AboutChuck = () => {
                         <li>Continuing to be fiscally responsible with taxpayer dollars.</li>
                     </ul>
                     </p>
-                <p><strong className="last-sentence" style={{fontSize: '1.4rem',}}>I will continue to serve our students, staff and community fairly and equitably, ensuring everyone has a seat at the table, while keeping student wellness and educational success at the forefront of all my decisions.</strong></p>
+                <p className="last-sentence"><strong>I will continue to serve our students, staff and community fairly and equitably, ensuring everyone has a seat at the table, while keeping student wellness and educational success at the forefront of all my decisions.</strong></p>
             </div>
 
         </section>
