af66ebe690d56943c2f53eb7189968d7c2173247
Kris Hvattum <klhvattum@live.com>
2023-09-06 14:44:16 -0700

    style hero text img

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,9 +7,6 @@ const Hero = () => {
         <section className="hero-section">
             <div className="hero-container">
                 <div className='hero-text'>
-                        {/* <h1>Experience</h1> 
-                        <h1 className='commitment'>Commitment</h1>
-                        <h1>Community</h1> */}
                         <img src={Sign} alt="Sign reading Re-Elect Chuck Whitfield for School Board - Monroe - District 2" />
                         <h1>Experience, Commitment, Community</h1>
 
