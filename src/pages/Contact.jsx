d8d6fa0c9b61c1393ce8c011667789c6a44e6cab
Kris Hvattum <klhvattum@live.com>
2023-09-15 16:27:06 -0700

    style email links on contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -9,7 +9,7 @@ const Contact = () => {
             <div style={{ width: '100%', backgroundColor: '#005EB0', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
                 <div className="contact-text-1">
                     <img src={Headshot} className='contact-headshot' />
-                    <p className='contact-text-p-1'>We value your input! Your feedback is important in shaping the future of our community's education, so Chuck welcomes your thoughts, concerns, and questions. Feel free to reach out via email at <a className='email-link' href='mailto:whitfieldforeducation@gmail.com' >WhitfieldForEducation@gmail.com</a> </p>
+                    <p className='contact-text-p-1'>We value your input! Your feedback is important in shaping the future of our community's education, so Chuck welcomes your thoughts, concerns, and questions. Feel free to send an <a className='email-link' href='mailto:whitfieldforeducation@gmail.com' >email</a>! </p>
                 </div>
             </div>
 
@@ -17,7 +17,7 @@ const Contact = () => {
             <h2 className='section-heading'>Support the Campaign</h2>
             <div className='contact-text'>
                 <p>
-                    Your support is greatly appreciated! To make a contribution and help us further our mission, please reach out to us via email at <a className='email-link' href='mailto:whitfieldforeducation@gmail.com'  >WhitfieldForEducation@gmail.com</a>. We'll be more than happy to provide you with the necessary guidance on how to donate. Your generosity fuels our campaign and makes a significant impact on our community. We extend our heartfelt gratitude for considering contributing to our cause. Together, we can make a positive change!
+                    Your support is greatly appreciated! To make a contribution and help us further our mission, please reach out to us via <a className='email-link' href='mailto:whitfieldforeducation@gmail.com'  >email</a>. We'll be more than happy to provide you with the necessary guidance on how to donate. Your generosity fuels our campaign and makes a significant impact on our community. We extend our heartfelt gratitude for considering contributing to our cause. Together, we can make a positive change!
                 </p>
             </div>
             <div className='contact-sign-div'>
