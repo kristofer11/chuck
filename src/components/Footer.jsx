2233194efb05fd6fea8c0a3ba840cdb98d5fe097
Kris Hvattum <klhvattum@live.com>
2023-09-16 21:31:40 -0700

    layout footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,18 +4,23 @@ import { Link } from 'react-router-dom';
 const Footer = () => {
     const handleNavigation = (id) => {
         const button = document.getElementById(id)
-        button.addEventListener('click', () => window.location.href= '/')
+        button.addEventListener('click', () => window.location.href = '/')
     }
 
     return (
-        <footer className='footer'>            
+        <footer className='footer'>
             <div className='copyright-div'>
-                <div className='email-div'>
-                    Email: <a href="mailto:whitfieldforeducation@gmail.com">WhitfieldForEducation@gmail.com</a> 
+                <a className='d-block d-md-none' href="https://www.facebook.com/profile.php?id=100094757426779" target="_blank">
+                    <img src={FbLogo} className='fb-logo-sm' />
+                </a>
+                <div className='email-div d-flex gap-2 align-items-center'>
+
+                    <div>Email: </div>
+                    <div><a href="mailto:whitfieldforeducation@gmail.com">WhitfieldForEducation@gmail.com</a></div>
                 </div>
                 {/* <span className='d-none d-md-block'>|</span> */}
                 <div className='copyright'>©2023 paid for by Chuck Whitfield</div>
-                <a href="https://www.facebook.com/profile.php?id=100094757426779" target="_blank">
+                <a className='d-none d-md-block' href="https://www.facebook.com/profile.php?id=100094757426779" target="_blank">
                     <img src={FbLogo} className='fb-logo' />
                 </a>
             </div>
