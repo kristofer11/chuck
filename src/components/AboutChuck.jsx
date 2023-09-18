32ece4ea7ac8e2dc48354d9efe7307c9e2632162
Kris Hvattum <klhvattum@live.com>
2023-09-18 13:44:09 -0700

    edit about page text per chucks revision

--- src/components/AboutChuck.jsx
+++ src/components/AboutChuck.jsx
@@ -20,26 +20,26 @@ const AboutChuck = () => {
                         {/* <p>I've lived in Monroe my entire life. I graduated from Monroe High School, enlisted into the Army, graduated from college with a business degree and currently operate a vehicle-licensing agency in Monroe. Through these stages of life I have learned how to be a leader who leads by example.</p>
                         <p>Over the last four years it has been my pleasure to serve as board director during some difficult times. During these hard times we worked together overcoming hardships and finding solutions to move forward. I've served on the curriculum committee, encouraging community input and choosing robust curriculum, and as the legislative representative, building relationships with lawmakers and advocating for our students.</p> */}
                         <p>
-                            As a devoted husband, father, and longtime resident of Monroe, I bring a rich history of community involvement and a deep commitment to our city.
+                            As a husband (39 years), father, and longtime resident of Monroe, I bring a history of community involvement and a solid commitment to our cherished city.
                         </p>
                         <p>
-                            My roots in this vibrant Pacific Northwest community run deep, having been born and raised right here in Monroe. I hold a special place in my heart for this town, and I'm proud to call it home. After completing my education at Monroe High School, I enlisted in the U.S. Army, where I had the honor of serving our country and gaining valuable life experiences.
+                            My roots in this community run deep, having been born and raised in Monroe. My journey began at Monroe High School, where I laid the foundation for my future. Following graduation, I served our country with honor in the U.S. Army, gaining invaluable life experiences.
                         </p>
 
                     </div>
                 </div>
                 <div className='main-text'>
                     <p>
-                        With a business degree in hand, I now operate a local vehicle-licensing agency in Monroe. However, my true passion has always been dedicated to the betterment of our beloved city.
+                        My educational pursuits led me to attain a business degree, and today, I operate a local vehicle-licensing agency in Monroe. However, beyond my professional endeavors, my heart has always been dedicated to the betterment of our beloved city.
                     </p>
                     <p>
-                        For the past four years, I've had the privilege of serving as a Monroe School District School Board Director. During this time, I've worked alongside fellow board members and educators, steering the district through various challenges. I actively participate in the curriculum committee, ensuring community input is valued in selecting educational programs. As the legislative representative, I've fostered relationships with lawmakers, advocating for the benefit of Monroe's students.
+                        Over the past four years, I've had the privilege of serving as a Monroe School District School Board Director. During this time, I've navigated the district through challenging moments, working collaboratively with my fellow board members and educators. My participation in the curriculum committee has allowed me to foster community input and help select robust educational programs. As the legislative representative, I've built meaningful relationships with lawmakers, advocating for the benefit of Monroe's students.
                     </p>
                     <p>
-                        My vision extends beyond the classroom; I'm an active member of the Monroe Rotary and the Monroe Chamber of Commerce, deeply rooted in our faith-based community. My service record includes past presidencies of the Snohomish Rotary and Duvall Chamber, reflecting my commitment to strengthening our city's social fabric.
+                        My vision for our community extends beyond the classroom. I'm a member of the Monroe Rotary and the Monroe Chamber of Commerce, engaged in our faith-based community. My service record includes past presidencies of the Snohomish Rotary and Duvall Chamber, demonstrating my commitment to strengthening the social fabric of our city.
                     </p>
                     <p>
-                        My values, dedication to education, and community service have led me to seek a role on the Monroe School Board. I'm always open to your questions, feedback, and support. Please feel free to reach out via email, and if you're interested in supporting my campaign, your contribution will be greatly appreciated. Together, we can work towards a brighter future for Monroe's students and our entire community.
+                        My values, my commitment to education, and my community service have led me to pursue a role on the Monroe School Board. I welcome your questions, feedback, and support. Please don't hesitate to reach out via email, and if you're interested in supporting my campaign, your contribution will be genuinely appreciated. Together, we can shape a brighter future for Monroe's students and our entire community.
                     </p>
                     <div className='family-link-div'>
                         <Link to='/whitfield-family' ><div className='w-family-link' id='w-family-link' ><em>Read more about the Whitfield family legcacy in Monroe</em></div></Link>
