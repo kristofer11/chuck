7c8f025b14db4edd360964eded4f15aef7c7c882
Kris Hvattum <klhvattum@live.com>
2023-09-18 20:27:21 -0700

    add donation button to contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,6 @@
 import Sign from '../assets/images/Chuck for Schoolboard 2023.png';
 import Headshot from '../assets/images/hero.jpg';
+import { Link } from 'react-router-dom';
 import FooterLinks from '../components/FooterLinks';
 
 
@@ -20,11 +21,15 @@ const Contact = () => {
                 <p>
                     Your support is greatly appreciated! To make a contribution and help us further our mission, please reach out to us via <a className='email-link' href="mailto:whitfieldforeducation@gmail.com?subject=Campaign Donation for Chuck Whitfield&body=I would like to support Chuck Whitfield's Monroe School District campaign. Please send me information on how to donate and/or get involved.">email</a>. We will promptly send instructions on how to contribute. Your generosity fuels our campaign and makes a significant impact on our community. We extend our heartfelt gratitude for considering contributing to our cause.
                 </p>
+
                 <img className='d-none d-lg-block' src={Sign} />
             </div>
-            <div className='contact-sign-div d-block d-lg-none'>
-                <img src={Sign} className='contact-sign' />
+            <div className='family-link-div mt-2 mb-4'>
+                <a href="mailto:whitfieldforeducation@gmail.com?subject=Campaign Donation for Chuck Whitfield&body=I would like to support Chuck Whitfield's Monroe School District campaign. Please send me information on how to donate and/or get involved." ><div className='w-family-link' id='w-family-link'><em>Donate to the campaign</em></div></a>
             </div>
+            {/* <div className='contact-sign-div d-block d-lg-none'>
+                <img src={Sign} className='contact-sign' />
+            </div> */}
         </section>
     )
 }
