a72f70dfbde8519ccd5fadd51785792446df8d55
Kris Hvattum <klhvattum@live.com>
2023-10-17 14:38:16 -0700

    reword heading on homepage video

--- src/components/HomeVideos.jsx
+++ src/components/HomeVideos.jsx
@@ -5,7 +5,7 @@ import '../styles/homeVideos.scss'
 const HomeVideos = () => {
   return (
     <div className='home-videos'>
-        <h2 className='video-title'>Chuck's Emphasis on Safety</h2>
+        <h2 className='video-title'>Emphasis on Safety</h2>
         <Video source={"https://www.youtube.com/embed/ZkDXwOKtSBc?si=7Eww5ulDR3vWFoPv"} />        
         <h2 className='video-title'>Community Engagement</h2>
         <Video source={"https://www.youtube.com/embed/B8TcQSWgru0?si=Wf-ibtMDsfMAeEjy"} />
