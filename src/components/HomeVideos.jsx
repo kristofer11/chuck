76581923112c446651e53bd62d6698909740e41c
Kris Hvattum <klhvattum@live.com>
2023-10-19 12:27:01 -0700

    fix video title placement

--- src/components/HomeVideos.jsx
+++ src/components/HomeVideos.jsx
@@ -5,9 +5,9 @@ import '../styles/homeVideos.scss'
 const HomeVideos = () => {
   return (
     <div className='home-videos'>
-        <h2 className='video-title'>Emphasis on Safety</h2>
-        <Video source={"https://www.youtube.com/embed/B8TcQSWgru0?si=Wf-ibtMDsfMAeEjy"} />   
         <h2 className='video-title'>Community Engagement</h2>
+        <Video source={"https://www.youtube.com/embed/B8TcQSWgru0?si=Wf-ibtMDsfMAeEjy"} />   
+        <h2 className='video-title'>Emphasis on Safety</h2>
         <Video source={"https://www.youtube.com/embed/ZkDXwOKtSBc?si=7Eww5ulDR3vWFoPv"} />        
     </div>
   )
