9ee003efb41863ad08b917092f7b5546b3820bb7
Kris Hvattum <klhvattum@live.com>
2023-10-20 13:15:03 -0700

    add animations to whitfield family history page

--- src/pages/WhitfieldFamily.jsx
+++ src/pages/WhitfieldFamily.jsx
@@ -3,70 +3,248 @@ import Mural from '../assets/images/mural.png';
 import MichaelGrad from '../assets/images/michaelGrad.png';
 import ChuckPolly from '../assets/images/chuckPolly.png';
 import OldMonroe from '../assets/images/oldMonroe.jpg';
+import { motion } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
 
 const WhitfieldFamily = () => {
+    const [ref1, inView1] = useInView({
+        triggerOnce: true,
+    })
+    const [ref2, inView2] = useInView({
+        triggerOnce: true,
+    })
+    const [ref3, inView3] = useInView({
+        triggerOnce: true,
+    })
+    const [ref4, inView4] = useInView({
+        triggerOnce: true,
+    })
+    const [ref5, inView5] = useInView({
+        triggerOnce: true,
+    })
+    const [ref6, inView6] = useInView({
+        triggerOnce: true,
+    })
+    const [ref7, inView7] = useInView({
+        triggerOnce: true,
+    })
+    const [ref8, inView8] = useInView({
+        triggerOnce: true,
+    })
+    const [ref9, inView9] = useInView({
+        triggerOnce: true,
+    })
+    const [ref10, inView10] = useInView({
+        triggerOnce: true,
+    })
+    const [ref11, inView11] = useInView({
+        triggerOnce: true,
+    })
+    const [ref12, inView12] = useInView({
+        triggerOnce: true,
+    })
+    const [ref13, inView13] = useInView({
+        triggerOnce: true,
+    })
+    const [ref14, inView14] = useInView({
+        triggerOnce: true,
+    })
+    const [ref15, inView15] = useInView({
+        triggerOnce: true,
+    })
+    const [ref16, inView16] = useInView({
+        triggerOnce: true,
+    })
+    const [ref17, inView17] = useInView({
+        triggerOnce: true,
+    })
+    const [ref18, inView18] = useInView({
+        triggerOnce: true,
+    })
+    const [ref19, inView19] = useInView({
+        triggerOnce: true,
+    })
+    const [ref20, inView20] = useInView({
+        triggerOnce: true,
+    })
+
     return (
         <article className='whitfield-family'>
             <div className='w-family-history-hero'>
                 {/* <h1 className='section-heading'>Whitfield Family History</h1> */}
 
             </div>
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={inView1 ? { opacity: 1 } : {}}
+                transition={{ duration: 0.5 }}
+                ref={ref1}
+            >
             <h1 className='section-heading'>Whitfield Family History</h1>
+            </motion.div>
             <section className='w-family-section-1 w-family-section'>
                 <div className='d-flex intro-block'>
+                <motion.div
+                initial={{ opacity: 0 }}
+                animate={inView2 ? { opacity: 1 } : {}}
+                transition={{ duration: 0.5 }}
+                ref={ref2}
+            >
                     <figure className='d-block d-md-none'>
                         <img src={ThreeGenerations} alt='Chuck Whitfield with grandson Lincoln Hvattum and his late father, Larry Whitfield' />
                         <figcaption style={{ color: 'black' }}>(Left to right) Larry Whitfield, Chuck Whitfield and Chuck's grandson, Lincoln.</figcaption>
                     </figure>
+                    </motion.div>
                     <div className='intro-block-text'>
+                    <motion.div
+                initial={{ opacity: 0 }}
+                animate={inView3 ? { opacity: 1 } : {}}
+                transition={{ duration: 0.5 }}
+                ref={ref3}
+            >
                         <p>For over 100 years, the Whitfield Family has faithfully served the Monroe Community. It all began with our Patriarch, Lawrence Whitfield, who arrived in Washington at only thirteen years old. His son, Larry Whitfield, who, until the age of 92 years old, was often found in the Whitfield Licensing office. Currently, Chuck Whitfield and his wife Nancy are the proprietors of the business. A commitment to our neighbors, to sound business, to developing relationships, to doing the right thing - no matter what - has always defined our family and will continue to do so in the years to come.  </p>
+                        </motion.div>
+                        <motion.div
+                initial={{ opacity: 0 }}
+                animate={inView4 ? { opacity: 1 } : {}}
+                transition={{ duration: 0.5 }}
+                ref={ref4}
+            >
                         <p>Born in 1902 in Pawnee, Nebraska, Lawrence moved with his family to Cathcart in 1915 where they settled on a seven-acre parcel of previously logged land. With this, the logging industry would find itself into the marrow of Lawrence's being, beginning at twelve when he worked a summer in California as a "whistle punk." Relaying instructions between the work site and the log landing via whistle bursts, he earned $5 a day. He remembers his first "paycheck" was four $20 gold pieces for a month of work. Later, he returned to Washington and worked greasing skids: using smaller logs to transport larger, more marketable logs dragged over them via horse or mule teams. </p>
+                        </motion.div>
                     </div>
+                    <motion.div
+                initial={{ opacity: 0 }}
+                animate={inView5 ? { opacity: 1 } : {}}
+                transition={{ duration: 0.5 }}
+                ref={ref5}
+            >
                     <figure className='d-none d-md-block'>
                         <img src={ThreeGenerations} alt='Chuck Whitfield with grandson Lincoln Hvattum and his late father, Larry Whitfield' />
                         <figcaption style={{ color: 'black' }}>(Left to right) Larry Whitfield, Chuck Whitfield and Chuck's grandson, Lincoln.</figcaption>
                     </figure>
-
+                    </motion.div>
                 </div>
-
             </section>
-            <section className='w-family-section-2 w-family-section'>
 
+            <section className='w-family-section-2 w-family-section'>
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={inView6 ? { opacity: 1 } : {}}
+                transition={{ duration: 0.5 }}
+                ref={ref6}
+            >
                 <p>In 1923, Lawrence arrived in the emerging community of Monroe, a booming timber town, to work for the Florence Acres Logging Company. He felled timber and worked as a high rigger. This was an extremely dangerous job requiring him to free climb tall trees with only a rope, chopping branches as he went, then topping the tree with his saw in one hand and only holding the trunk with the other. At 5:00pm every night, a train would pull into Monroe to be loaded with logs on their way to Everett and on.</p>
+                </motion.div>
+                <motion.div
+                initial={{ opacity: 0 }}
+                animate={inView7 ? { opacity: 1 } : {}}
+                transition={{ duration: 0.5 }}
+                ref={ref7}
+            >
                 <p>In 1927, Lawrence married a local Snohomish girl, Marion Parkhurst, whose parents owned a large dairy farm. As the logging industry began to decline in the area, Lawrence, Marion, and their new family adapted to the changing region.</p>
+                </motion.div>
 
             </section>
             <section className='w-family-section-3 w-family-section'>
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={inView8 ? { opacity: 1 } : {}}
+                transition={{ duration: 0.5 }}
+                ref={ref8}
+            >
                 <figure>
                     <img src={Mural} />
                     <figcaption style={{ color: 'black' }}>A portion of the mural that adorns the wall at Whitfield's Auto licensing depicting Laurence Whitfield (center).</figcaption>
                 </figure>
-
+</motion.div>
             </section>
             <section className='w-family-section-4 w-family-section'>
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={inView9 ? { opacity: 1 } : {}}
+                transition={{ duration: 0.5 }}
+                ref={ref9}
+            >
                 <p>1948 marked a turning point in Lawrence's career. Though his education never exceeded the eighth grade, Lawrence was a true pioneer with the kind of entrepreneurial spirit that came to define the western frontier. With grit and determination, he began a real estate business, then an insurance business, both of which became highly successful endeavors and served the growing community of Monroe, as well as other towns now springing up in the Snohomish Valley and beyond. </p>
+                </motion.div>
+                <motion.div
+                initial={{ opacity: 0 }}
+                animate={inView10 ? { opacity: 1 } : {}}
+                transition={{ duration: 0.5 }}
+                ref={ref10}
+            >
                 <p>
                     In his spare time, Lawrence was an avid outdoorsman and hunter. Every year, he would travel north to hunt deer, elk, and moose outside Clinton, B.C. Hunted and taxidermized in 1948, Pete the Moose was an exceptionally large specimen of some 1,400 pounds named in honor of one of Lawrence's favorite guides. Today, you will find Pete the Moose hanging in the Whitfield Licensing office. Pete has been a much-loved part of the Whitfield office for over 70 years, occasionally refurbished, and has become the mascot for Whitfield Licensing.
                 </p>
+                </motion.div>
+                <motion.div
+                initial={{ opacity: 0 }}
+                animate={inView11 ? { opacity: 1 } : {}}
+                transition={{ duration: 0.5 }}
+                ref={ref11}
+            >
                 <p>Lawrence left behind a tremendous legacy of hard work, unassailable ethics, and shrewd business acumen. Truly a servant leader, he could recall a time when social services didn't exist and when those in need turned to the goodness of their friends and neighbors for help. He believed in being the kind of friend and neighbor that could be counted on, and it is this legacy, more than any other, that he passed down to his children and grandchildren.</p>
+            </motion.div>
             </section>
             <section className='w-family-section-5 w-family-section'>
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={inView12 ? { opacity: 1 } : {}}
+                transition={{ duration: 0.5 }}
+                ref={ref12}
+            >
                 <p>Lawrence's son, Larry, eventually joined the family business in 1950, expanding it into local communities all around the sound. Before the agency was sold, there were seven regional offices from Snohomish County to Whatcom County, and Larry spear-headed the addition of a new division in 1979: Vehicle Licensing. Like his father, Larry believed that the primary role of the business was to serve the community, so whether it was personal insurance for farmers or business insurance for hundreds of local Western Washington companies, it was his goal to meet personally with each client, discuss their needs, and help them choose the best options. But his commitment to serving the community didn't stop at the office's front doors. A dedicated member of the Lion's Club and a school board member for ten years, Larry believed deeply in giving back to the town that had given him so much. Larry was a Korean War Army Veteran.</p>
+                </motion.div>
+                <motion.div
+                initial={{ opacity: 0 }}
+                animate={inView13 ? { opacity: 1 } : {}}
+                transition={{ duration: 0.5 }}
+                ref={ref13}
+            >
                 <p>Eventually settling on a farm just outside Monroe, Larry and his wife, Darlene, raised their children in the place they, themselves, had grown up. The second of four siblings, Chuck remembers his early years in agriculture, his mother's help to win State Farmer of the Year, and the foundation of leadership and organization that helped build his life. After graduating high school, Chuck spent three years in the Army, stationed in New Ulm Germany, then returned to Western Washington to attend college and join the family business.</p>
+                </motion.div>
             </section>
             <section className='w-family-section-6 w-family-section'>
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={inView14 ? { opacity: 1 } : {}}
+                transition={{ duration: 0.5 }}
+                ref={ref14}
+            >
                 <figure>
                     <img src={MichaelGrad} alt="Chuck and Nancy Whitfield at their son Michael's graduation" />
                     <figcaption>Chuck and Nancy at their son Michael's college graduation.</figcaption>
                 </figure>
-
+</motion.div>
             </section>
             <section className='w-family-section-7 w-family-section'>
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={inView15 ? { opacity: 1 } : {}}
+                transition={{ duration: 0.5 }}
+                ref={ref15}
+            >
                 <p>In 1984, Chuck returned to Monroe, married local resident Nancy Rinks, and began selling insurance with his father. For almost 30 years, the ethics and community commitment of both his grandfather and his father shaped his business practices. In 1990, he partnered with another prominent local businessman to bring the Rotary Club to Monroe, and since then, they've overseen community outreach projects and fundraising efforts for over three decades. </p>
+                </motion.div>
+                <motion.div
+                initial={{ opacity: 0 }}
+                animate={inView16 ? { opacity: 1 } : {}}
+                transition={{ duration: 0.5 }}
+                ref={ref16}
+            >
                 <p>In 2012, Chuck sold the insurance business, but not content to remain idle, he immediately began pursuing a new career. In 2014, he joined the Whitfield Licensing branch, beginning alongside his own staff as a trainee and putting in the thousands of hours necessary to become certified for the Department of Licensing in the state of Washington. Since then, he has continued to serve the Monroe community in a variety of capacities, beginning in 2019, on the board of the Monroe School District. He is also a member of the Monroe Chamber of Commerce and remains active in Rotary, where he is most proud of their partnership with the Monroe/Sky Valley YMCA to offer Miracle League, a baseball field for athletes who have disabilities. He is also proud of their commitment to the Monroe Police Department's K-9 Unit. In 2018, they partnered to raise funds for the purchase of German Shepherd TANGO to replace their previous K-9. </p>
+                </motion.div>
             </section>
             <section className='w-family-section-8 w-family-section'>
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={inView17 ? { opacity: 1 } : {}}
+                transition={{ duration: 0.5 }}
+                ref={ref17}
+            >
                 <p>The Whitfield Legacy winds back over 100 years, a century of living and working in the Monroe community, of raising their children here and seeing them grow up to give back to the place they love so much. Even as Monroe continues to change and evolve, agriculture now giving way to the Gateway of the Cascade mountains and increased agritourism, it remains true to its roots, as well. The railroad still runs through town, though now as apt to carry airplane wings as local logging produce. Evergreen State Fair continues to show the community's deep commitment to animal husbandry and its history of homesteading skills; and the Whitfield family continues to raise up this once small town, even as it has raised another generation to follow in their forebears' footsteps.</p>
+                </motion.div>
             </section>
             {/* <figure className='chuck-polly'>
                 <img src={ChuckPolly} alt='Chuck Whitfield holding his golden retreiver puppy, Polly' />
@@ -74,15 +252,35 @@ const WhitfieldFamily = () => {
             </figure> */}
             <section className='w-family-section-9 w-family-section'>
                 <div className='d-flex outro-block align-items-center flex-md-nowrap flex-wrap'>
+                <motion.div
+                initial={{ opacity: 0 }}
+                animate={inView18 ? { opacity: 1 } : {}}
+                transition={{ duration: 0.5 }}
+                ref={ref18}
+            >
                     <figure className='chuck-polly'>
                         <img src={ChuckPolly} alt='Chuck Whitfield holding his golden retreiver puppy, Polly' />
                         <figcaption style={{ color: 'black' }}>Chuck holding his golden retreiver, Polly</figcaption>
                     </figure>
+                    </motion.div>
                     <div className='intro-block-text'>
-
+                    <motion.div
+                initial={{ opacity: 0 }}
+                animate={inView19 ? { opacity: 1 } : {}}
+                transition={{ duration: 0.5 }}
+                ref={ref19}
+            >
                         <p>Today, Chuck continues to run Whitfield Licensing with the help of his son, Michael. His three children, Lacey, Michael, and Kendra, remain in Monroe and they are excited to see a fourth generation take up the reins of the family's deep commitment to serving their friends and neighbors. </p>
+                        </motion.div>
+                        <motion.div
+                initial={{ opacity: 0 }}
+                animate={inView20 ? { opacity: 1 } : {}}
+                transition={{ duration: 0.5 }}
+                ref={ref20}
+            >
                         <p>The Whitfield Legacy has always been about more than just names, dates, and shingles on the side of buildings. The true Whitfield Legacy is their deep commitment to the Monroe community, their ethic of service, and their love for their fellow human. They remain proud of their many years in Monroe and Snohomish County and look forward to many more still to come.
                         </p>
+                        </motion.div>
                     </div>
 
 
