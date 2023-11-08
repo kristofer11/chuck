7bf018948d8e0bd222ac11742e6dadf6138866ed
Kris Hvattum <klhvattum@live.com>
2023-11-08 11:06:03 -0800

    move analytics function call in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,12 @@ import './index.scss';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { inject } from '@vercel/analytics';
 
+    inject();
+    
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
-    inject();
+
         <App />    
     </Router>
 
