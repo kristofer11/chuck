2fd4f96fdd32cbfeb9b3908b88fd25d9f99716b9
Kris Hvattum <klhvattum@live.com>
2023-12-14 13:45:32 -0800

    remove vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.scss';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { inject } from '@vercel/analytics';
-
-    inject();
     
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
